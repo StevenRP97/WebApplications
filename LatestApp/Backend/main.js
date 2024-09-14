16c81b38b5188a2e09f177b5a4c3501f03ae3ea0
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-14 17:45:55 -0600

    Putting everything up to date

--- LatestApp/Backend/main.js
+++ LatestApp/Backend/main.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const users = require('users');
+const users = require('./users');
 
 const app = express();
-app.use(bodyParser);
-// GET endpoint
 
 app.get("/main", (req,res)=>{
-	var appending = '';
-	for (let i = 0; i > users.length; i++){
-		appending = appending + ``;
-	}
-	var xd = ``
-	res.send(<h1>These are the employees</h1>);
+  var appending = '';
+  for (let i = 0; i > users.length; i++){
+    appending = appending + ``;
+  }
+  var xd = ``
+  res.send('<h1>These are the employees</h1' + appending);
 })
 
 app.post('/users', (req, res) => {
-	users.
+  console.log("XD")
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
