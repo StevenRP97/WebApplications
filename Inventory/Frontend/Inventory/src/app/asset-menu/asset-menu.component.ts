53f40c14faf355116672074f4f84d40419a34fbe
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-22 21:18:57 -0600

    Connecting Asset Menu with AssetAPI

--- Inventory/Frontend/Inventory/src/app/asset-menu/asset-menu.component.ts
+++ Inventory/Frontend/Inventory/src/app/asset-menu/asset-menu.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-asset-menu',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './asset-menu.component.html',
   styleUrl: './asset-menu.component.css'
 })
 export class AssetMenuComponent {
+  // Constructor and some required variables
+  constructor(private http: HttpClient){}
   selectedComponent: string = 'AssignMenu';
+  assetUrl: string = 'http://localhost:3000/';
+  public data: any[] = [];
+
   showComponent(component:string){
     this.selectedComponent = component;
+  }
+
+  ngOnInit():void{
+    this.GetAllAssets();
+  }
+
+  public GetAllAssets(){
+    this.http.get(this.assetUrl).subscribe((resp:any)=>{
+     this.data = resp;
+     console.log(this.data)
+    })
+
   }
 }
